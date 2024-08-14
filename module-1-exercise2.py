90608531905dccdd6248d8d06f771406516638d4
Tmwland <164696980+Tmwland@users.noreply.github.com>
2024-08-14 17:22:52 -0400

    Create module-1-exercise2.py

---
+++ module-1-exercise2.py
@@ -0,0 +1,5 @@
+income = 250_000
+lowtaxland_rate = 0.05
+ripoffland_rate = 0.43
+
+print('Your income is', 250_000, 'and you would pay', 250_000 * lowtaxland_rate, 'income tax in Lowtaxland or', 250_000 * ripoffland_rate, 'income tax in Ripoffland.', 'You would save', 250_000 * ripoffland_rate - 250_000 * lowtaxland_rate, 'by paying taxes in Lowtaxland!')
